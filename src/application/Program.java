ef5b1cbe5c234f265df6f4ef9d40d838db43184c
Marcos Vinicius <marcosvmorais49@yahoo.com.br>
2024-10-11 11:24:54 -0300

    Atualizando repositorio apos fazer um git reset e voltar alguns commits

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,17 +1,29 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
     public static void main(String[] args) {
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21,"bob","bob@gmail.com",new Date(), 3000.0, obj);
+        System.out.println("=== TEST 1: seller findById =====");
+        Seller seller = sellerDao.findById(3);
         
         System.out.println(seller);
+        
+        System.out.println("=== TEST 2: seller findByDepartment =====");
+        Department department = new Department(2,null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        for (Seller obj : list) {
+            System.out.println(obj);
+        }
         
     }
     
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import java.util.List;
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -9,5 +10,6 @@ public interface SellerDao {
     public void update(Seller obj);
     public void deletById(Integer id);
     public Seller findById(Integer id);   
-    public List<Seller> findAll();    
+    public List<Seller> findAll(); 
+    public List<Seller> findByDepartment(Department department);
 }
